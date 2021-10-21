78999d4df0ce3f7248620303598b223a75dd3e50
AP <ashis.n.patel@gmail.com>
2021-10-20 22:15:08 -0500

    Troubleshoot service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
 const FILES_TO_CACHE = [
     './index.html',
     './css/styles.css',
+    './js/idb.js',
     './js/index.js',
-    './js/idb.js'
 ];
 
 const APP_PREFIX = 'BudgetTracker-';
@@ -12,9 +12,9 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 // service worker's load prior to the window object, so event listeners will be called on 'self' or the service worker
 
 // install service worker and add files to the cache storage
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CACHE_NAME).then(function (cache) {
             console.log(`Installing cache: ${CACHE_NAME}`);
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -24,15 +24,13 @@ self.addEventListener('install', (event) => {
 // activate service worker
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((keyList) => {
-            console.log({keyList});
-            let cacheKeepList = keyList.filter(key => key.indexOf(APP_PREFIX))
-            console.log({cacheKeepList});
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function(key){return key.indexOf(APP_PREFIX)} )
             cacheKeepList.push(CACHE_NAME)
 
             return Promise.all(
-                keyList.map((key,i) => {
-                    if(cacheKeepList.indexOf(key) === -1) {
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
                         console.log(`deleting cache : ${keyList[i]}`);
                         return caches.delete(keyList[i]);
                     }
@@ -42,16 +40,16 @@ self.addEventListener('activate', (event) => {
     )
 })
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function (event) {
     console.log(`fetch request : ${event.request.url}`);
     event.respondWith(
-        caches.match(event.request).then(request => {
+        caches.match(event.request).then(function (request) {
             if (request) {
                 console.log(`responding with cache : ${event.request.url}`);
                 return request;
             } else {
                 console.log(`file is not cached, fetching : ${event.request.url}`);
-                return fetch(event.request); 
+                return fetch(event.request);
             }
         })
     )
